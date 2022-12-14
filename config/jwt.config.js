68cee57b4bc61159ae07bfb9e300bb4ba1651ca1
andredcp <andre.dacostap3@gmail.com>
2022-12-14 02:35:32 +0100

    jwt and user route adjustments

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export function generateToken(user) {
-  const { _id, name, email, role } = user;
+  const { _id, name, email, role, profileImg } = user;
 
   const signature = process.env.TOKEN_SIGN_SECRET;
   const expiration = "12h";
 
-  return jwt.sign({ _id, name, email, role }, signature, {
+  return jwt.sign({ _id, name, email, role, profileImg }, signature, {
     expiresIn: expiration,
   });
 }
